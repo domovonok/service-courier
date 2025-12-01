07e5c411860d773b05374de61609d790dff6572e
domovonok <domovonokk@gmail.com>
2025-12-01 22:57:47 +0300

    cleanup small fix

--- tests/testdb/cleanup.go
+++ tests/testdb/cleanup.go
@@ -3,13 +3,23 @@ package testdb
 import (
 	"context"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+var psql = sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
+
 func CleanupTables(ctx context.Context, pool *pgxpool.Pool) error {
-	_, err := pool.Exec(ctx, `
-		TRUNCATE TABLE delivery CASCADE;
-		TRUNCATE TABLE couriers CASCADE;
-	`)
-	return err
+	tables := []string{"delivery", "couriers"}
+
+	for _, table := range tables {
+		sql, args, _ := psql.Delete(table).ToSql()
+
+		_, err := pool.Exec(ctx, sql, args...)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
